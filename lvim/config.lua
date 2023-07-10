910e3284b3ab1d48dba62a549ec8bc525f344c0f
231tr0n <56352048+231tr0n@users.noreply.github.com>
2023-07-11 01:42:46 +0530

    Update config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -101,16 +101,6 @@ lvim.plugins = {
     end,
   },
   {
-    'wfxr/minimap.vim',
-    build = "cargo install --locked code-minimap",
-    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-    config = function()
-      vim.cmd("let g:minimap_width = 10")
-      vim.cmd("let g:minimap_auto_start = 1")
-      vim.cmd("let g:minimap_auto_start_win_enter = 1")
-    end,
-  },
-  {
     "tpope/vim-surround",
     config = function()
       vim.o.timeoutlen = 500
