b672c7473533c2cf4bc00c926bfec4a824e42db4
231tr0n <zeltronsrikar@gmail.com>
2023-10-26 09:26:11 +0530

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ local function clone_paq()
   local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
   local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
   if not is_installed then
-    vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+    vim.fn.system({ "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path })
     return true
   end
 end
@@ -16,15 +16,49 @@ local function bootstrap_paq(packages)
   end
   paq(packages)
   paq.install()
+  paq.update()
+  paq.clean()
 end
 
-bootstrap_paq {
+bootstrap_paq({
   "savq/paq-nvim",
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  "windwp/nvim-ts-autotag",
-}
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  "nvim-treesitter/nvim-treesitter-context",
+  "nvim-treesitter/nvim-treesitter-refactor",
+  "tpope/vim-surround",
+  "tpope/vim-commentary",
+  "tpope/vim-ragtag",
+  "tpope/vim-unimpaired",
+  "tpope/vim-fugitive",
+  "tpope/vim-endwise",
+  "tpope/vim-characterize",
+  "tpope/vim-repeat",
+  "tpope/vim-git",
+  "tpope/vim-sleuth",
+  "tpope/vim-eunuch",
+  "tpope/vim-classpath",
+  "tpope/vim-speeddating",
+  "echasnovski/mini.pairs",
+  "nvim-lua/plenary.nvim",
+  "folke/tokyonight.nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  "lukas-reineke/indent-blankline.nvim",
+  "junegunn/fzf.vim",
+  "folke/trouble.nvim",
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  "jay-babu/mason-nvim-dap.nvim",
+  "theHamsta/nvim-dap-virtual-text",
+})
 
-require 'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
   ensure_installed = {
     "awk",
     "bash",
@@ -91,7 +125,7 @@ require 'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     disable = function(lang, buf)
-      local max_filesize = 100 * 1024
+      local max_filesize = 1 * 1024 * 1024
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
@@ -108,15 +142,165 @@ require 'nvim-treesitter.configs'.setup {
       node_decremental = "grm",
     },
   },
+  refactor = {
+    highlight_definitions = {
+      enable = true,
+      clear_on_cursor_move = true,
+    },
+    highlight_current_scope = { enable = true },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = "grr",
+      },
+    },
+    navigation = {
+      enable = true,
+      keymaps = {
+        goto_definition = "gnd",
+        list_definitions = "gnD",
+        list_definitions_toc = "gO",
+        goto_next_usage = "<a-*>",
+        goto_previous_usage = "<a-#>",
+      },
+    },
+  },
   indent = {
-    enable = true
-  },
-  autotag = {
     enable = true,
-    filetypes = { "html", "xml", "php", "vue", "svelte" },
   },
+})
+
+require('mini.pairs').setup()
+require('ibl').setup()
+
+local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+    vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+  end
+})
+
+local default_setup = function(server)
+  lspconfig[server].setup({})
+end
+
+local dap = require('dap')
+require("dapui").setup()
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = { default_setup },
+})
+require("mason-nvim-dap").setup({
+  automatic_installation = true,
+})
+
+require("nvim-dap-virtual-text").setup()
+
+local cmp = require('cmp')
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Enter key confirms completion item
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+    -- Ctrl + space triggers completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'fileformat' },
+    lualine_y = { 'filetype' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
 
+-- options
+vim.opt.undofile = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
+vim.g.mapleader = " "
+vim.cmd("colorscheme tokyonight-storm")
+local pwd = vim.fn.system("git rev-parse --show-toplevel 2> /dev/null")
+vim.cmd("cd" .. pwd)
+
+-- keymaps
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
