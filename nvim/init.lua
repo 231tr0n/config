febe3346c4ad157ed31135f142e8b49bda3a5551
231tr0n <56352048+231tr0n@users.noreply.github.com>
2023-12-20 12:20:25 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -493,6 +493,17 @@ require("conform").setup({
 	},
 	lsp_fallback = true,
 })
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_fallback = true, range = range })
+end, { range = true })
 
 -- dap setup
 local dap = require("dap")
@@ -558,7 +569,7 @@ require("ollama").setup({
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.undofile = false
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.number = true
