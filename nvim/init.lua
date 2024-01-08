01fc55cb6b09f900d47a2fd9ada50e076752b16b
231tr0n <zeltronsrikar@gmail.com>
2024-01-08 16:01:32 +0530

    config

--- nvim/init.lua
+++ nvim/init.lua
@@ -542,16 +542,7 @@ require("conform").setup({
 	lsp_fallback = true,
 })
 vim.api.nvim_create_user_command("Format", function(args)
-	-- local range = nil
-	-- if args.count ~= -1 then
-	-- 	local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-	-- 	range = {
-	-- 		start = { args.line1, 0 },
-	-- 		["end"] = { args.line2, end_line:len() },
-	-- 	}
-	-- end
-	-- require("conform").format({ async = true, lsp_fallback = true, range = range })
-	require("conform").format({ async = true, lsp_fallback = true })
+	require("conform").format({ lsp_fallback = true })
 end, { range = true })
 
 -- dap setup
