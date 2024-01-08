165d44806707f97ff4ef917a7129f648834ebd43
231tr0n <zeltronsrikar@gmail.com>
2024-01-08 10:27:51 +0530

    config

--- nvim/init.lua
+++ nvim/init.lua
@@ -589,6 +589,52 @@ for _, language in ipairs({ "typescript", "javascript" }) do
 		},
 	}
 end
+dap.adapters.gdb = {
+	id = "gdb",
+	type = "executable",
+	command = "gdb",
+	args = { "--quiet", "--interpreter=dap" },
+}
+dap.configurations.c = {
+	{
+		name = "Run executable (GDB)",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			local path = vim.fn.input({
+				prompt = "Path to executable: ",
+				default = vim.fn.getcwd() .. "/",
+				completion = "file",
+			})
+			return (path and path ~= "") and path or dap.ABORT
+		end,
+	},
+	{
+		name = "Run executable with arguments (GDB)",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			local path = vim.fn.input({
+				prompt = "Path to executable: ",
+				default = vim.fn.getcwd() .. "/",
+				completion = "file",
+			})
+			return (path and path ~= "") and path or dap.ABORT
+		end,
+		args = function()
+			local args_str = vim.fn.input({
+				prompt = "Arguments: ",
+			})
+			return vim.split(args_str, " +")
+		end,
+	},
+	{
+		name = "Attach to process (GDB)",
+		type = "gdb",
+		request = "attach",
+		processId = require("dap.utils").pick_process,
+	},
+}
 
 -- ai setup
 require("ollama").setup({
