df102ace6da90f383cb9d4a407c5c987f12b9c50
231tr0n <56352048+231tr0n@users.noreply.github.com>
2024-01-13 11:54:28 +0530

    Update wsl_initial_setup.bash

--- wsl/arch/wsl_initial_setup.bash
+++ wsl/arch/wsl_initial_setup.bash
@@ -9,4 +9,3 @@ passwd zeltron
 
 echo -e "\e[32mRun the following command to set zeltron as default user 'Arch.exe config --default-user zeltron'\e[0m"
 echo -e "\e[32mReboot the computer if user not set as default or run the following command 'net stop lxssmanager && net start lxssmanager'\e[0m"
-exit
