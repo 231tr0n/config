a9556e9326e6bf23511413f8fcb200f002b57a0d
231tr0n <zeltronsrikar@gmail.com>
2024-01-13 11:18:26 +0530

    test

--- lang-setup-conf/java/java-debug.bash
+++ lang-setup-conf/java/java-debug.bash
@@ -4,9 +4,9 @@ rm -rf java-debug
 git clone https://github.com/microsoft/java-debug.git
 cd java-debug
 ./mvnw clean install
-install -Dm755 "com.microsoft.java.debug.plugin/target/*.jar" \
-               "/usr/share/java-debug/com.microsoft.java.debug.plugin.jar"
-install -Dm755 "com.microsoft.java.debug.core/target/*.jar" \
-               "/usr/share/java-debug/com.microsoft.java.debug.core.jar"
+sudo install -Dm755 "$(ls com.microsoft.java.debug.plugin/target/*.jar)" \
+  "/usr/share/java-debug/com.microsoft.java.debug.plugin.jar"
+sudo install -Dm755 "$(ls com.microsoft.java.debug.core/target/*.jar)" \
+  "/usr/share/java-debug/com.microsoft.java.debug.core.jar"
 cd ..
 rm -rf java-debug
