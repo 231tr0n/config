3c95b529c58e0e6fb35128ea69eb17a5d49cdef3
231tr0n <zeltronsrikar@gmail.com>
2024-01-13 11:44:04 +0530

    test

--- wsl/arch/wsl_main_setup.bash
+++ wsl/arch/wsl_main_setup.bash
@@ -34,6 +34,10 @@ curl https://raw.githubusercontent.com/231tr0n/config/main/fish/fish_variables -
 curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_prompt.fish -o ~/.config/fish/functions/fish_prompt.fish
 curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_mode_prompt.fish -o ~/.config/fish/functions/fish_mode_prompt.fish
 curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/java/java-debug.bash -o ~/scripts/java-debug.bash
+curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/java/java-lsp.bash -o ~/scripts/java-lsp.bash
+
+chmod +x ~/scripts/java-debug.bash
+chmod +x ~/scripts/java-lsp.bash
 
 echo -e "\e[32mRun the command 'chsh' and set fish shell\e[0m"
 echo -e "\e[32mRun the command 'ssh-keygen -t rsa' and generate key-pair for ssh and scp\e[0m"
