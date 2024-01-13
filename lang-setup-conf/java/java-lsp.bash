265d150f174b7d37793798ffd668d5c844838fd2
231tr0n <zeltronsrikar@gmail.com>
2024-01-13 11:36:21 +0530

    config

---
+++ lang-setup-conf/java/java-lsp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+rm -rf java-lsp
+mkdir -p java-lsp
+cd java-lsp
+wget https://www.eclipse.org/downloads/download.php?file=/jdtls/snapshots/jdt-language-server-latest.tar.gz -O jdtls.tar.gz
+tar -zxvf jdtls.tar.gz
+sudo rm -rf /usr/share/java/jdtls
+sudo mkdir -p /usr/share/java/jdtls
+sudo cp -R config_* features plugins bin /usr/share/java/jdtls
+sudo mkdir -p /usr/bin
+sudo ln -s --relative "/usr/share/java/jdtls/bin/jdtls" "/usr/bin/jdtls"
+cd ..
+rm -rf java-lsp
