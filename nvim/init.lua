3d3433070b1f002d3878c97f5e51876bec96219e
231tr0n <56352048+231tr0n@users.noreply.github.com>
2024-01-13 23:01:11 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,6 +94,7 @@ bootstrap_paq({
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-nvim-lua",
 	"hrsh7th/cmp-calc",
+	"hrsh7th/cmp-cmdline",
 	"hrsh7th/cmp-nvim-lsp",
 	"L3MON4D3/LuaSnip",
 	"saadparwaiz1/cmp_luasnip",
@@ -344,6 +345,22 @@ cmp.setup({
 			luasnip.lsp_expand(args.body)
 		end,
 	},
+})
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	completion = { autocomplete = false },
+	sources = {
+		{ name = "buffer" },
+	},
+})
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	completion = { autocomplete = false },
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 require("neodev").setup()
