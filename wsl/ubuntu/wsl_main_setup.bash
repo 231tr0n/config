ad8691273a43069ed346f25609d919e2754f537f
231tr0n <zeltronsrikar@gmail.com>
2024-01-13 18:04:57 +0530

    test

--- wsl/ubuntu/wsl_main_setup.bash
+++ wsl/ubuntu/wsl_main_setup.bash
@@ -3,7 +3,7 @@
 cd ~
 
 sudo apt update && sudo apt upgrade && sudo apt autoremove --purge
-sudo apt install git curl wget neofetch openssl ssh man-db htop
+sudo apt install git curl wget neofetch openssl ssh man-db htop jq
 
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
@@ -23,21 +23,67 @@ curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_m
 curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/java/java-debug.bash -o ~/scripts/java-debug.bash
 curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/java/java-lsp.bash -o ~/scripts/java-lsp.bash
 curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/go/go-install.bash -o ~/scripts/go-install.bash
+curl https://raw.githubusercontent.com/231tr0n/config/main/lang-setup-conf/lua/lua-lsp.bash -o ~/scripts/lua-lsp.bash
 
 chmod +x ~/scripts/java-debug.bash
 chmod +x ~/scripts/java-lsp.bash
 chmod +x ~/scripts/go-install.bash
+chmod +x ~/scripts/lua-lsp.bash
 
-cd ~/scripts
-./java-debug.bash
-./java-lsp.bash
-./go-install.bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo bash -
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update && sudo apt upgrade
+
+sudo apt install python-is-python3 python3-pip nodejs vim tmux clang clang-tools gcc make cmake meson maven ninja-build openjdk-21-jdk openjdk-17-jdk openjdk-11-jdk luajit luarocks
+sudo update-java-alternatives -s java-1.21.0-openjdk-amd64
+sudo npm install -g npm@latest
+sudo npm install -g typescript
+cd ~/scripts
+./go-install.bash
 cd ~
 
-sudo apt install python-is-python3 python3-pip pipx nodejs
+sudo apt install clangd
+go install -v golang.org/x/tools/gopls@latest
+sudo npm install -g vscode-langservers-extracted@latest
+sudo npm install -g yaml-language-server@latest
+sudo npm install -g bash-language-server@latest
+cd ~/scripts
+./java-lsp.bash
+./lua-lsp.bash
+cd ~
 
+sudo apt install gdb lldb
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+cd ~/scripts
+./java-debug.bash
+cd ~
+
+sudo apt install clang-tidy shellcheck checkstyle checkstyle-doc
+sudo luarocks install luacheck
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ~/.local/bin
+pip install pylint yamllint
+sudo npm install -g jsonlint@latest
+
+sudo apt install clang-format jq libxml2-utils
+pip install black
+sudo npm install -g google-java-format@latest
+sudo npm install -g prettier@latest
+go install github.com/google/yamlfmt/cmd/yamlfmt@latest
+go install mvdan.cc/gofumpt@latest
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
+cargo install stylua
+cargo install stylua --features lua52
+cargo install stylua --features lua53
+cargo install stylua --features lua54
+cargo install stylua --features luau
+
+cargo install git-delta ripgrep fd-find bob-nvim bat
+
+bob use latest
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 echo -e "\e[32mRun the command 'chsh' and set fish shell\e[0m"
 echo -e "\e[32mRun the command 'ssh-keygen -t rsa' and generate key-pair for ssh and scp\e[0m"
