c6fc42261487ea740d6397eb89052ca8175ae235
231tr0n <zeltronsrikar@gmail.com>
2024-01-13 18:41:37 +0530

    test

---
+++ lang-setup-conf/xml/lua-lsp.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+rm -rf xml-lsp
+mkdir -p xml-lsp
+cd xml-lsp
+
+wget "https://www.eclipse.org/downloads/download.php?file=/lemminx/snapshots/org.eclipse.lemminx-uber.jar" -O lemminx.jar
+cat > wrapper << 'EOL'
+#!/bin/sh
+
+java \
+    -noverify \
+    -Xms1G \
+    -jar /usr/share/java/lemminx/lemminx.jar
+EOL
+
+sudo install -Dm644 lemminx.jar /usr/share/java/lemminx/lemminx.jar
+sudo install -Dm755 wrapper /usr/bin/lemminx
+
+cd ..
+rm -rf xml-lsp
