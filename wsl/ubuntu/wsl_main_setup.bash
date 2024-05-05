5c5930138d3ed2c22b555064186c20714abdf99e
231tr0n <zeltronsrikar@gmail.com>
2024-05-05 08:12:06 +0000

    test

--- wsl/ubuntu/wsl_main_setup.bash
+++ wsl/ubuntu/wsl_main_setup.bash
@@ -57,7 +57,7 @@ sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update -y
 sudo apt install -y git
 
-sudo apt install -y python-is-python3 python3-pip python3-venv nodejs clang clang-tools gcc make cmake meson maven ninja-build openjdk-11-source openjdk-21-source openjdk-17-source openjdk-21-jdk openjdk-17-jdk openjdk-11-jdk openjdk-11-doc openjdk-17-doc openjdk-21-doc luajit luarocks texlive texlive-base texlive-science texlive-science-doc universal-ctags pipx texlive-extra-utils texlive-latex-extra evince ffmpeg
+sudo apt install -y python-is-python3 python3-pip python3-venv nodejs clang clang-tools gcc make cmake meson maven ninja-build openjdk-11-source openjdk-21-source openjdk-17-source openjdk-21-jdk openjdk-17-jdk openjdk-11-jdk openjdk-11-doc openjdk-17-doc openjdk-21-doc luajit luarocks texlive texlive-base texlive-science texlive-science-doc universal-ctags pipx texlive-extra-utils texlive-latex-extra evince ffmpeg mpv
 sudo update-java-alternatives -s java-1.21.0-openjdk-amd64
 sudo npm install -g npm@latest
 sudo npm install -g typescript@latest
