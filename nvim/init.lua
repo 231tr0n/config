75d93db51b1fa2c78da341bae9c9d7df34faef79
231tr0n <zeltronsrikar@gmail.com>
2024-08-28 18:18:16 +0000

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -565,6 +565,13 @@ now(function()
 		modules = {},
 		indent = {
 			enable = true,
+			disable = function(lang, buf)
+				local max_filesize = 2 * 1024 * 1024
+				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
 		},
 		incremental_selection = {
 			enable = false,
