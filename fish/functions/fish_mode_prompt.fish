17e0e138686800542417cc415881c3f06e48686e
231tr0n <zeltronsrikar@gmail.com>
2024-10-03 19:49:00 +0000

    test

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -2,17 +2,17 @@ function fish_mode_prompt
     set_color -o
     switch $fish_bind_mode
         case default
-            echo (set_color brcyan)'['(set_color yellow)'N'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color yellow)'N'(set_color cyan)']'(set_color yellow)':'
         case insert
-            echo (set_color brcyan)'['(set_color green)'I'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color green)'I'(set_color cyan)']'(set_color yellow)':'
         case replace_one
-            echo (set_color brcyan)'['(set_color red)'r'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color red)'r'(set_color cyan)']'(set_color yellow)':'
         case visual
-            echo (set_color brcyan)'['(set_color brmagenta)'V'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color magenta)'V'(set_color cyan)']'(set_color yellow)':'
         case replace
-            echo (set_color brcyan)'['(set_color red)'R'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color red)'R'(set_color cyan)']'(set_color yellow)':'
         case '*'
-            echo (set_color brcyan)'['(set_color blue)'r'(set_color brcyan)']'(set_color yellow)':'
+            echo (set_color cyan)'['(set_color blue)'r'(set_color cyan)']'(set_color yellow)':'
     end
     set_color normal
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,14 @@
 function fish_prompt
     set_color -o
-    echo (set_color brcyan)'['(set_color brblue)(whoami)(set_color red)'@'(set_color brblue)(prompt_hostname)(set_color brcyan)']'
-    echo (set_color brcyan)'['(set_color brblue)(prompt_pwd)(set_color brcyan)']'
+    echo (set_color cyan)'['(set_color blue)(whoami)(set_color red)'@'(set_color blue)(prompt_hostname)(set_color cyan)']'
+    echo (set_color cyan)'['(set_color blue)(prompt_pwd)(set_color cyan)']'
     if string length --quiet (fish_git_prompt)
-      echo (set_color brcyan)'['(set_color brblue)(fish_git_prompt | xargs)(set_color brcyan)']'
+      echo (set_color cyan)'['(set_color blue)(fish_git_prompt | xargs)(set_color cyan)']'
     end
     if fish_is_root_user
-        echo -n (set_color brcyan)'['(set_color red)'#'(set_color brcyan)']'(set_color yellow)': '
+        echo -n (set_color cyan)'['(set_color red)'#'(set_color cyan)']'(set_color yellow)': '
     else
-        echo -n (set_color brcyan)'['(set_color red)'$'(set_color brcyan)']'(set_color yellow)': '
+        echo -n (set_color cyan)'['(set_color red)'$'(set_color cyan)']'(set_color yellow)': '
     end
     set_color normal
 end
