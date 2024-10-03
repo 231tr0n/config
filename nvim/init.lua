eedfe9bf1191a9c2f8057afba19766a950bfde9c
231tr0n <zeltronsrikar@gmail.com>
2024-10-03 19:24:44 +0000

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -158,7 +158,7 @@ now(function()
 	vim.g.everforest_ui_contrast = "low"
 	vim.g.everforest_float_style = "dim"
 	vim.g.everforest_better_performance = 1
-	vim.g.everforest_enable_italic = 1
+	vim.g.everforest_enable_italic = 0
 	vim.g.everforest_disable_italic_comment = 0
 	vim.g.everforest_transparent_background = 0
 	vim.g.everforest_dim_inactive_windows = 1
