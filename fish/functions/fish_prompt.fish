f58746e00f130b79e63f996b6f9df655cac90496
231tr0n <zeltronsrikar@gmail.com>
2024-10-20 08:36:19 +0000

    test

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
     echo (set_color cyan)'['(set_color blue)(whoami)(set_color red)'@'(set_color blue)(prompt_hostname)(set_color cyan)']'
     echo (set_color cyan)'['(set_color blue)(prompt_pwd)(set_color cyan)']'
     if string length --quiet (fish_git_prompt)
-      echo (set_color cyan)'['(set_color blue)(fish_git_prompt | xargs)(set_color cyan)']'
+        echo (set_color cyan)'['(set_color blue)(fish_git_prompt | xargs)(set_color cyan)']'
     end
     if fish_is_root_user
         echo -n (set_color cyan)'['(set_color red)'#'(set_color cyan)']'(set_color yellow)': '
