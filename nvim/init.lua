dca50cbd4b422f15368ce0bc58d6493c5ae484da
231tr0n <zeltronsrikar@gmail.com>
2024-11-28 17:39:19 +0000

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -1134,16 +1134,6 @@ later(function()
 			port = 5005,
 		},
 	}
-	dap.configurations.lua = {
-		{
-			type = "nlua",
-			request = "attach",
-			name = "Attach to running Neovim instance",
-		},
-	}
-	dap.adapters.nlua = function(callback, config)
-		callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-	end
 	dap.adapters.lldb = {
 		type = "executable",
 		command = (function()
