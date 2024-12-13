8a2c45d4ff430695ab1c4ef0aca55f0d90228ad8
231tr0n <zeltronsrikar@gmail.com>
2024-12-13 02:48:58 +0000

    test

--- kubernetes/kubernetes.bash
+++ kubernetes/kubernetes.bash
@@ -9,6 +9,5 @@ go install -v sigs.k8s.io/kind@latest
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm -f kubectl
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-go install -v github.com/kubernetes/kompose@latest
 cd ..
 rm -rf kubernetes
