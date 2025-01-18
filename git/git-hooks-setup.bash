245f54b8c2dc98f7bb762fe6aaee4f1e4b239c83
231tr0n <zeltronsrikar@gmail.com>
2025-01-18 19:33:23 +0000

    test

--- git/git-hooks-setup.bash
+++ git/git-hooks-setup.bash
@@ -3,54 +3,53 @@
 set -exuo pipefail
 
 setup() {
-	if ! command -v git; then
-		echo "Git is not installed in your system. Exitting."
-		exit 1
-	fi
-	if [ "$(git rev-parse --show-toplevel)" == "" ]; then
-		echo "Your are not in a git initialised repository. Exitting."
-		exit 1
-	fi
+  if ! command -v git; then
+    echo "Git is not installed in your system. Exitting."
+    exit 1
+  fi
+  if [ "$(git rev-parse --show-toplevel)" == "" ]; then
+    echo "Your are not in a git initialised repository. Exitting."
+    exit 1
+  fi
 
-	local git_root
-	local hooks_dir
-	local commit_msg_hook
-	local pre_commit_hook
+  local git_root
+  local hooks_dir
+  local commit_msg_hook
+  local pre_commit_hook
 
-	git_root=$(git rev-parse --show-toplevel)
-	hooks_dir=$git_root/.git-hooks
-	commit_msg_hook=$hooks_dir/commit-msg
-	pre_commit_hook=$hooks_dir/pre-commit
+  git_root=$(git rev-parse --show-toplevel)
+  hooks_dir=$git_root/.git-hooks
+  commit_msg_hook=$hooks_dir/commit-msg
+  pre_commit_hook=$hooks_dir/pre-commit
 
-	if ! test -d "$hooks_dir"; then
-		mkdir "$hooks_dir"
-	fi
-	if ! test -f "$hooks_dir/commit-msg"; then
-		touch "$hooks_dir"/commit-msg
-	fi
-	if ! test -f "$hooks_dir/pre-commit"; then
-		touch "$hooks_dir"/pre-commit
-	fi
+  if ! test -d "$hooks_dir"; then
+    mkdir "$hooks_dir"
+  fi
 
-	chmod +x "$hooks_dir"/commit-msg
-	chmod +x "$hooks_dir"/pre-commit
+  if ! test -f "$hooks_dir/commit-msg"; then
+    touch "$hooks_dir"/commit-msg
+    cat >"$commit_msg_hook" <<-'EOF'
+			#!/bin/bash
 
-	cat >"$commit_msg_hook" <<-'EOF'
-		#!/bin/bash
+			if ! grep -Eq '^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-z -]+\))?!?: [a-z0-9 ]+$' "$1"; then
+				echo "Prompt does not follow conventional commiting style(https://www.conventionalcommits.org/). Aborting the commit."
+				exit 1
+			fi
+		EOF
+  fi
+  chmod +x "$hooks_dir"/commit-msg
 
-		if ! grep -Eq '^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-z -]+\))?!?: [a-z0-9 ]+$' "$1"; then
-		  echo "Prompt does not follow conventional commiting style(https://www.conventionalcommits.org/). Aborting the commit."
-		  exit 1
-		fi
-	EOF
+  if ! test -f "$hooks_dir/pre-commit"; then
+    touch "$hooks_dir"/pre-commit
+    cat >"$pre_commit_hook" <<-'EOF'
+			#!/bin/bash
 
-	cat >"$pre_commit_hook" <<-'EOF'
-		#!/bin/bash
+			# Different pre commit hooks to run.
+		EOF
+  fi
+  chmod +x "$hooks_dir"/pre-commit
 
-		# Different pre commit hooks to run.
-	EOF
-
-	git config --local core.hooksPath "$hooks_dir"
+  git config --local core.hooksPath "$hooks_dir"
 }
 
-setup
+setup "$@"
