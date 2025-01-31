c148224f81a7a241f5f339617f3c9e389522f6c8
231tr0n <zeltronsrikar@gmail.com>
2025-01-31 01:16:33 +0000

    test

--- wsl/ubuntu/wsl_main_setup.bash
+++ wsl/ubuntu/wsl_main_setup.bash
@@ -44,6 +44,7 @@ chmod +x ~/scripts/xml-lsp.bash
 chmod +x ~/scripts/node-debug.bash
 chmod +x ~/scripts/python-debug.bash
 chmod +x ~/scripts/kubernetes.bash
+chmod +x ~/scripts/neovim.bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH=$PATH:$HOME/.cargo/bin
