2270a5db3d56c2965c11f424beb5b0e06155cb1b
231tr0n <zeltronsrikar@gmail.com>
2025-04-30 18:56:34 +0000

    test

--- wsl/archlinux/wsl.bash
+++ wsl/archlinux/wsl.bash
@@ -2,46 +2,28 @@
 
 set -e
 
-# WARNING Don't edit the parameters here
-SYSTEM_USERNAME="zeltron"
-SYSTEM_PASSWORD="zeltron"
-ROOT_PASSWORD="$SYSTEM_PASSWORD"
-SSH_KEY_PASSWORD="$SYSTEM_PASSWORD"
+DEFAULT_USERNAME="zeltron"
+DEFAULT_USER_PASSWORD="zeltron"
+ROOT_PASSWORD="$DEFAULT_USER_PASSWORD"
+SSH_KEY_PASSWORD="$DEFAULT_USER_PASSWORD"
 WSL_CONFIG_CHANGED="No"
 
-function error_log {
-  echo
-  echo
-  echo -ne "[\e[1;31mERROR\e[1;0m] " >&2
-  echo -e "$1" >&2
-  echo
-  echo
-}
-function info_log {
-  echo
-  echo
-  echo -ne "[\e[1;36mINFO\e[1;0m] "
-  echo -e "$1"
-  echo
-  echo
-}
-
-while getopts 'r:u:p:h' opt; do
+while getopts 's:r:u:p:h' opt; do
   case "$opt" in
   r)
     ROOT_PASSWORD="$OPTARG"
     ;;
   p)
-    SYSTEM_PASSWORD="$OPTARG"
+    DEFAULT_USER_PASSWORD="$OPTARG"
     ;;
   u)
-    SYSTEM_USERNAME="$OPTARG"
+    DEFAULT_USERNAME="$OPTARG"
     ;;
   s)
     SSH_KEY_PASSWORD="$OPTARG"
     ;;
   h | ?)
-    info_log "Help\n
+    printf "Help\n
     -s: ssh-key password
     -r: root password
     -u: system username
@@ -51,6 +33,14 @@ while getopts 'r:u:p:h' opt; do
     ;;
   esac
 done
+
+function sudo_cmd {
+  if [ "$(id -u)" -eq 0 ]; then
+    "$@"
+  else
+    sudo -S "$@" <<<"$DEFAULT_USER_PASSWORD"
+  fi
+}
 
 if grep "default=" /etc/wsl.conf >/dev/null && grep "guiApplications=" /etc/wsl.conf >/dev/null; then
   WSL_CONFIG_CHANGED="Yes"
@@ -64,7 +54,7 @@ if [ "$WSL_CONFIG_CHANGED" = "No" ]; then
 
   if ! grep "default=" /etc/wsl.conf >/dev/null; then
     WSL_CONFIG_CHANGED="Yes"
-    printf "\n\n[user]\ndefault=%s" "$SYSTEM_USERNAME" >>/etc/wsl.conf
+    printf "\n\n[user]\ndefault=%s" "$DEFAULT_USERNAME" >>/etc/wsl.conf
   fi
   if ! grep "guiApplications=" /etc/wsl.conf >/dev/null; then
     WSL_CONFIG_CHANGED="Yes"
@@ -80,30 +70,24 @@ if [ "$WSL_CONFIG_CHANGED" = "No" ]; then
 
   echo "$ROOT_PASSWORD" | passwd -s
 
-  useradd -m -s /usr/bin/fish -G wheel "$SYSTEM_USERNAME"
+  useradd -m -s /usr/bin/fish -G wheel "$DEFAULT_USERNAME"
 
-  echo "$SYSTEM_PASSWORD" | passwd -s "$SYSTEM_USERNAME"
+  passwd -s "$DEFAULT_USERNAME" <<<"$DEFAULT_USER_PASSWORD"
 
-  info_log "WSL config changed. Run 'wsl --terminate archlinux' and restart it again"
-  exit
+  su - "$DEFAULT_USERNAME"
 fi
 
-if [ "$(id -u)" -eq 0 ]; then
-  error_log "Please don't run this script as super user"
-  exit 1
-fi
+cd "$HOME"
+sudo_cmd pacman -Syu --noconfirm --needed git base-devel reflector
 
-cd ~
-sudo pacman -Syu --noconfirm --needed git base-devel reflector
+sudo_cmd mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo_cmd reflector --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo_cmd pacman -Syu --noconfirm --needed
 
-sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Syu --noconfirm --needed
-
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
+git clone https://aur.archlinux.org/yay-bin.git "$HOME/yay-bin"
+cd "$HOME/yay-bin"
 makepkg -sic --noconfirm
-cd ..
+cd "$HOME"
 rm -rf yay-bin
 
 mkdir -p ~/.config
@@ -121,24 +105,20 @@ curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_p
 curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_user_key_bindings.fish -o ~/.config/fish/functions/fish_user_key_bindings.fish
 curl https://raw.githubusercontent.com/231tr0n/config/main/fish/functions/fish_mode_prompt.fish -o ~/.config/fish/functions/fish_mode_prompt.fish
 
-export PATH=$PATH:$HOME/.local/share/coursier/bin
-export PATH=$PATH:$HOME/.cargo/bin
-export PATH=$PATH:/usr/local/go/bin
-
-echo "$SYSTEM_PASSWORD" | yay -Syu --sudoloop --noconfirm --needed go jdk-openjdk python python-pip python-pipx curl wget ca-certificates openssl openssh inxi htop man-db jq vim neovim tree-sitter-cli tmux tmate libgit2 fuse rustup docker docker-buildx docker-compose bat fzf fd ripgrep lsd fastfetch nodejs-lts npm clang gcc typescript luajit texlive ts-node delve python-debugpy lldb gdb make cmake meson maven gradle ninja luarocks woff2 ctags ffmpeg mpv zoxide evince net-tools sysstat axel tldr ncdu firefox chromium bash-completion shellcheck checkstyle luacheck python-pylint yamllint sqlfluff coursier java-debug jdtls metals bloop pyright basedpyright-bin yaml-language-server sql-language-server svelte-language-server eslint-language-server lua-language-server typescript-language-server bash-language-server dockerfile-language-server vim-language-server lemminx vtsls marksman vscode-html-languageserver vscode-css-languageserver vscode-json-languageserver vscode-js-debug-bin tidy libxml2 golangci-lint-langserver-bin golangci-lint-bin eslint python-black yamlfmt gofumpt golines shfmt stylua yamlfix google-java-format git-delta hurl cargo-update diff-so-fancy gup lazygit python-pylatexenc nodejs-nodemon ollama kubectl minikube helm
+echo "$DEFAULT_USER_PASSWORD" | yay -Syu --sudoloop --noconfirm --needed go jdk-openjdk python python-pip python-pipx curl wget ca-certificates openssl openssh inxi htop man-db jq vim neovim tree-sitter-cli tmux tmate libgit2 fuse rustup docker docker-buildx docker-compose bat fzf fd ripgrep lsd fastfetch nodejs-lts npm clang gcc typescript luajit texlive ts-node delve python-debugpy lldb gdb make cmake meson maven gradle ninja luarocks woff2 ctags ffmpeg mpv zoxide evince net-tools sysstat axel tldr ncdu firefox chromium bash-completion shellcheck checkstyle luacheck python-pylint yamllint sqlfluff coursier java-debug jdtls metals bloop pyright basedpyright-bin yaml-language-server sql-language-server svelte-language-server eslint-language-server lua-language-server typescript-language-server bash-language-server dockerfile-language-server vim-language-server lemminx vtsls marksman vscode-html-languageserver vscode-css-languageserver vscode-json-languageserver vscode-js-debug-bin tidy libxml2 golangci-lint-langserver-bin golangci-lint-bin eslint python-black yamlfmt gofumpt golines shfmt stylua yamlfix google-java-format git-delta hurl cargo-update diff-so-fancy gup lazygit python-pylatexenc nodejs-nodemon ollama kubectl minikube helm
 
 rustup update stable
 
 cs setup -y
 
-sudo groupadd docker
-sudo usermod -aG docker "$USER"
+sudo_cmd groupadd docker
+sudo_cmd usermod -aG docker "$USER"
 
-sudo systemctl enable --now docker
-sudo systemctl enable --now ollama
+sudo_cmd systemctl enable docker
+sudo_cmd systemctl enable ollama
 
 if ! test -d "$HOME/.ssh"; then
-  printf "\n$SSH_KEY_PASSWORD\n$SSH_KEY_PASSWORD\n" | ssh-keygen -t rsa
+  printf "\n%s\n%s\n" "$SSH_KEY_PASSWORD" "$SSH_KEY_PASSWORD" | ssh-keygen -t rsa
 fi
 
 nvim --headless -c +'lua MiniDeps.update(nil, { force = true })' +TSUpdateSync +qa
