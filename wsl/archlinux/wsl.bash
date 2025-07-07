6cba07b8bd4b26909bf3157a6baadf1bd9383e83
231tr0n <zeltronsrikar@gmail.com>
2025-07-07 10:48:09 +0530

    test

--- wsl/archlinux/wsl.bash
+++ wsl/archlinux/wsl.bash
@@ -328,6 +328,7 @@ VSCODE_PLUGINS=(
 	"github.github-vscode-theme"
 	"github.copilot"
 	"github.copilot-chat"
+	"redhat.vscode-community-server-connector"
 )
 
 default_user_cmd "printf \"%s\n\" ${VSCODE_PLUGINS[*]} | DONT_PROMPT_WSL_INSTALL=No_Prompt_please xargs -I {} code --install-extension {} --force"
